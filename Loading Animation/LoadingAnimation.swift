d2f88ae238c47e7159606a43b2962a971a6d8ff1
ItayLador <158850420+ItayLador2013@users.noreply.github.com>
2025-02-03 22:43:49 -0600

    commit

---
+++ Loading Animation/LoadingAnimation.swift
@@ -0,0 +1,34 @@
+//
+//  LoadingAnimation.swift
+//
+//  Created by Itay Lador on 1/31/25.
+//
+
+import SwiftUI
+
+struct LoadingAnimation : View {
+    @State private var degree:Int = 270
+    @State private var spinnerLength = 0.6
+    
+    func animate() {
+        withAnimation(Animation.smooth(duration: 1.5).repeatForever(autoreverses: true)) {
+            degree = 270 + 360
+        }
+        withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: true)) {
+            spinnerLength = 0
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .trim(from: 0.0,to: spinnerLength)
+                .stroke(LinearGradient(colors: [.selectedOrange, .selectedYellow,.blue], startPoint: .topLeading, endPoint: .bottomTrailing),style: StrokeStyle(lineWidth: 8.0,lineCap: .round,lineJoin:.round))
+                .frame(width: 60,height: 60)
+                .rotationEffect(Angle(degrees: Double(degree)))
+                .onAppear{
+                    animate()
+                }
+        }
+    }
+}

251c89a6ca21a54908bdd15be74331d2ccc5ef82
ItayLador <158850420+ItayLador2013@users.noreply.github.com>
2025-02-20 13:49:39 -0600

    Update IndependentScrollView.swift

--- Independent ScrollView/IndependentScrollView.swift
+++ Independent ScrollView/IndependentScrollView.swift
@@ -16,15 +16,25 @@ struct IndependentScrollViewGrid<ListItemType, Label> : View where ListItemType 
     @State private var visible_index : Int = 0
     @State private var latest_index : Int = 0
     let expanded : ListItemType?
+    @State var container_width : CGFloat
 
 
-    init(list : [ListItemType], item_size: CGSize, container_width : CGFloat = UIScreen.main.bounds.width, expanded : ListItemType? = nil, @ViewBuilder _ label: @escaping (ListItemType) -> Label){
+    init(list : [ListItemType], item_size: CGSize, container_width c_width : CGFloat = UIScreen.main.bounds.width, expanded : ListItemType? = nil, @ViewBuilder _ label: @escaping (ListItemType) -> Label){
         self.list = list
         self.item_view = label
         self.item_size = item_size
-        self.num_items_in_row = Int(floor(container_width / item_size.width))
-        self.padding = (container_width - ( item_size.width * CGFloat(num_items_in_row))) / 4
+        self.container_width = c_width
+        self.num_items_in_row = Int(floor(c_width / item_size.width))
+        self.padding = (c_width - ( item_size.width * CGFloat(num_items_in_row))) / 4
         self.expanded = expanded
+    }
+    
+    func offset_x(index : Int) -> CGFloat {
+        return item_size.width * CGFloat(index % self.num_items_in_row) + (padding * CGFloat(index % self.num_items_in_row)) + (container_width - ( item_size.width * CGFloat(self.num_items_in_row - 1) + (padding * CGFloat(self.num_items_in_row - 1)) + item_size.width)  ) / 2.0
+    }
+    
+    func container_height() -> CGFloat {
+        return item_size.height * CGFloat((list.count / num_items_in_row)) + (padding * CGFloat(list.count / num_items_in_row)) + window.height * 0.25 + CGFloat(70)
     }
     
     var body: some View {
@@ -34,8 +44,8 @@ struct IndependentScrollViewGrid<ListItemType, Label> : View where ListItemType 
                     if latest_index <= index || abs(index - visible_index) <= 200 {
                         self.item_view(list_item)
                             .offset(
-                                x:  expanded?.id == list_item.id ? 0 : item_size.width * CGFloat(index % self.num_items_in_row) + (padding * CGFloat(index % self.num_items_in_row)),
-                                y: item_size.height * CGFloat(index / self.num_items_in_row) + (padding * CGFloat(index / self.num_items_in_row))
+                                x:  expanded?.id == list_item.id ? 0 : offset_x(index: index),
+                                y: item_size.height * CGFloat(index / self.num_items_in_row) + (padding * CGFloat(index / self.num_items_in_row)) + 70
                             )
                             .onAppear {
                                 if latest_index < index {
@@ -46,7 +56,7 @@ struct IndependentScrollViewGrid<ListItemType, Label> : View where ListItemType 
                     }
                 }
             }
-            .frame(width: window.width, height: item_size.width * CGFloat((list.count / num_items_in_row) + 3) + (padding * CGFloat(list.count / num_items_in_row)), alignment: .topLeading )
+            .frame(width: container_width, height: container_height(), alignment: .topLeading )
             .background(GeometryReader { geometry in
                 let yOffset = -geometry.frame(in: .named("IndependentScrollViewGrid")).origin.y
                 Color.clear.preference(key: ViewOffsetKey.self, value: yOffset)
